41124d15907071f970d2edbd66d70fee13b14a09
GitMurf <64155612+GitMurf@users.noreply.github.com>
2026-01-19 23:48:43 -0800

    deps: replace DefinitelyTyped @types/chrome with chrome-types official google types

--- src/shared/types/chrome.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * sidePanel.close() was added in Sept 2025 but not added to the @types/chrome yet
- * @see https://developer.chrome.com/docs/extensions/reference/api/sidePanel?hl=en#method-close
- */
-declare namespace chrome.sidePanel {
-  export function close(options: { windowId: number }): Promise<void>;
-}
